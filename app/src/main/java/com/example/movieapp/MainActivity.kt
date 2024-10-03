c0a0fa62f27ac1564dced3b48cfd663abd28369d
Ulises Mendoza <neodiletante@gmail.com>
2024-10-02 19:47:49 -0600

    2 oct home

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +36,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                MainContent(70.dp)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit){
+    MovieAppTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Movies") },
+                    colors = TopAppBarDefaults.topAppBarColors()
+                        .copy(containerColor = Color.Magenta)
+                )
+            },
+            ) {
+
+
+
+
+
+            MainContent(it.calculateTopPadding())
+
+        }
+    }
 }
+
+//
+// @Preview(showBackground = true)
+@Composable
+fun MainContent(topPadding : Dp, movieList: List<String> = listOf(
+    "Avatar", "300", "Harry Potter", "Life"
+)){
+    Column(
+        modifier = Modifier.padding(top = topPadding),
+        ){
+            Text(text = "Hello", style = TextStyle(color = Color.Black,
+                fontSize = 30.sp))
+
+    }
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
-    }
+    MyApp { MainContent(70.dp) }
 }
